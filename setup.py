e53f85d770370b6bb99862db64b76d93fde87ff7
Hanfei Sun <hfsun.tju@gmail.com>
2013-03-14 19:49:13 +0800

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Pyflow',
+    version='0.1',
+    packages=['pyflow', 'pyflow.testsuite'],
+    url='https://bitbucket.org/hanfeisun/pyflow',
+    license='MIT',
+    author='Hanfei Sun',
+    author_email='hfsun.tju@gmail.com',
+    description='A Python framework for writing flexible workflows'
+)
