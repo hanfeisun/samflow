000f11264495619024b5bb0c83041d007ff1eeb7
Hanfei Sun <hfsun.tju@gmail.com>
2013-03-19 21:29:30 +0800

    Add more friendly exception tips for LazyCollector

--- pyflow/helper.py
+++ pyflow/helper.py
@@ -2,16 +2,33 @@ class Lazy(object):
     def __init__(self, ref_obj, action_wrapper):
         self._obj = ref_obj
         self._wrapper = action_wrapper
+
     def fetch(self):
         return self._wrapper(self._obj)
+
+    def fake(self):
+        return "[not init]"
+
 class LazyCollector(Lazy):
     def __init__(self, ref_obj_container, **args):
         self._obj_container = ref_obj_container
         self._wrapper = {}
         for k, v in args.items():
             self._wrapper[k] = v
+
     def fetch(self):
-        return {k: [v(obj_i) for obj_i in self._obj_container] for k,v in self._wrapper.items()}
+        ret_dict = {}
+        for k, v in self._wrapper.items():
+            ret_dict[k] = []
+            for obj_i in self._obj_container:
+                try:
+                    ret_dict[k].append(v(obj_i))
+                except:
+                    print(k, v, obj_i)
+                    raise
+
+        return ret_dict
+
 
 
 
@@ -24,3 +41,11 @@ def fetch(lazy_obj):
         return [fetch(i) for i in lazy_obj]
     return lazy_obj
 
+def fake_fetch(lazy_obj):
+    if isinstance(lazy_obj, Lazy):
+        return lazy_obj.fake()
+    if isinstance(lazy_obj, dict):
+        return {k: fake_fetch(v) for k,v in lazy_obj.items()}
+    if isinstance(lazy_obj, list):
+        return [fake_fetch(i) for i in lazy_obj]
+    return lazy_obj
