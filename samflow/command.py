15b5eae5f3cc1e14eb593b97194cf7355e6f00bf
Hanfei Sun <hfsun.tju@gmail.com>
2013-05-07 19:18:20 +0800

    fix the expanduser part for

--- samflow/command.py
+++ samflow/command.py
@@ -329,14 +329,14 @@ def which(program):
 
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-    program = os.path.expanduser(program)
+
     fpath, fname = os.path.split(program)
     if fpath:
         if is_exe(program):
             return program
     else:
         for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
+            path = os.path.expanduser(os.path.strip('"'))
             exe_file = os.path.join(path, program)
             if is_exe(exe_file):
                 return exe_file
